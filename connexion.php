7b7369450314a213308e0157da6703928c36c602
Benjamin De Loof <bdeloof@gmail.com>
2023-09-05 21:25:27 +0200

    Création des pages de la navbar

--- connexion.php
+++ connexion.php
@@ -24,7 +24,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item active">
+                        <li class="nav-item">
                             <a class="nav-link" href="index.php">Accueil</a>
                         </li>
                         <li class="nav-item">
@@ -36,7 +36,7 @@
                         <li class="nav-item">
                             <a class="nav-link" href="contactus.php">Contactez nous !</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item active">
                             <a class="nav-link" href="connexion.php">Espace Collaborateurs</a>
                         </li>
                     </ul>
@@ -49,35 +49,16 @@
 <!-- --------------------------- MAIN --------------------------- -->
 
     <main>
-        <div class="container">
-            <div class="m-5">
-                <h1 class="title-site"><b>Bienvenue au garage Vincent Parrot !</b></h1>
-                <br>
-                <div class="garage-presentation">
-                    <p class="text-presentation">
-                        Fort d’une quinzaine d’année d’expérience dans la réparation automobile, le garage Vincent Parrot a ouvert ses portes à Toulouse en 2021. Nous nous engageons à vous offrir un service exceptionnel avec des réparations de qualité et une expérience client que vous ne trouverez pas ailleurs. Que vous ayez besoin d’une simple révision, d’une vidange ou d’une réparation de votre carrosserie, nous nous engageons à répondre à toutes vos exigences. Notre personnel compétent et amical se fera un plaisir de répondre à toutes vos questions sur les différentes réparations à amener à votre véhicule et vous tenir informer de l’avancée de vos réparations. Vous retrouverez également tout un panel de véhicule d’occasion à la vente révisés et mises en conformité par nos soins. Pour toutes informations, n’hésitez pas à nous contacter.
-                    </p>
-                    <img src="./src/img/auto-repair-3691962_1280.jpg" width="50%" height="50%" alt="Image de garagiste">
-                </div>
-            </div>
-        </div>
-        
-
-        <h2 class="title-site">Ils nous ont fait confiance</h2>
-
-        <div class="container section-confiance">
-            <img src="./src/img/flèche gauche.png" width="25px" height="25px" alt="Flèche dirigée vers la gauche">
-
-            <!-- Ajouter les avis -->
-
-            <img src="./src/img/flèche droite.png" width="25px" height="25px" alt="Flèche dirigée vers la droite">
-
-            <br>
-
-            <div class="container btn-donner-avis">
-                <button type="button" class="btn btn-danger">Donnez votre avis</button>
-            </div>
-        </div>
+      <h2 class="title-site">Consultez notre catalogue de véhicule d'occasion</h2>
+      <div class="container section-confiance">
+          <img src="./src/img/flèche gauche.png" width="25px" height="25px" alt="Flèche dirigée vers la gauche">
+          <!-- Ajouter les avis -->
+          <img src="./src/img/flèche droite.png" width="25px" height="25px" alt="Flèche dirigée vers la droite">
+          <br>
+          <div class="container btn-donner-avis">
+              <button type="button" class="btn btn-danger">Donnez votre avis</button>
+          </div>
+      </div>
     </main>
 
 
